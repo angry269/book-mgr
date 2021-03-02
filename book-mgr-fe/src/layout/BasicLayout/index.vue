b7c67f512721dd4cadec01b8e68018f9bb37968d
unknown <angry269>
2021-03-02 17:37:12 +0800

    完成基础布局

---
+++ book-mgr-fe/src/layout/BasicLayout/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="basic-layout">
+        <div class="app-header">
+           <div class="left">
+               <div class="title">设备管理系统</div>
+           </div>
+           <div class="right">
+               <div class="hello-msg">你好，xxx</div>
+               <div class="logout">退出</div>
+           </div>
+        </div>
+        <div class="app-header-placleholder"></div>
+         <div class="app-context">
+            <div class="left">
+               <app-nav />
+            </div>
+           <div class="right">
+              <router-view />
+            </div>
+        </div>
+    </div>
+</template>
+<script src='./index.js'></script>
+<style lang="scss" scoped>
+@import "./index.scss";
+</style>
